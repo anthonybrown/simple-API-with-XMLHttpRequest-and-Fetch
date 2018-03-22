12323f4cc588c8140bf22c92561fa5d821d0254f
Tony Brown <tony@tonybrown.me>
2018-03-21 20:16:38 -0400

    comment out the XMLHTTPRequest part of the code

--- js/app.js
+++ js/app.js
@@ -44,35 +44,35 @@ fetch(url)
     app.appendChild(errorMessage)
   })
 
-const request = new XMLHttpRequest();
+// const request = new XMLHttpRequest();
 
-request.open('GET', 'https://ghibliapi.herokuapp.com/films', true);
+// request.open('GET', 'https://ghibliapi.herokuapp.com/films', true);
 
-request.onload = function () {
-  const data = JSON.parse(this.response);
+// request.onload = function () {
+//   const data = JSON.parse(this.response);
 
-  if (request.status >= 200 && request.status < 400) {
-    data.forEach(movie => {
-      const card = document.createElement('div');
-      card.setAttribute('class', 'card');
+//   if (request.status >= 200 && request.status < 400) {
+//     data.forEach(movie => {
+//       const card = document.createElement('div');
+//       card.setAttribute('class', 'card');
 
-      const h1 = document.createElement('h1');
-      h1.textContent = movie.title;
+//       const h1 = document.createElement('h1');
+//       h1.textContent = movie.title;
 
-      const p = document.createElement('p');
-      movie.description = movie.description.substring(0, 300);
-      p.textContent = `${movie.description}...`;
+//       const p = document.createElement('p');
+//       movie.description = movie.description.substring(0, 300);
+//       p.textContent = `${movie.description}...`;
 
-      container.appendChild(card);
-      card.appendChild(h1);
-      card.appendChild(p);
-    })
-  } else {
-      const errorMessage = document.createElement('marquee');
-      errorMessage.textContent = 'Sorry, failed to fetch data...';
-      app.appendChild(errorMessage);
-  }
-}
+//       container.appendChild(card);
+//       card.appendChild(h1);
+//       card.appendChild(p);
+//     })
+//   } else {
+//       const errorMessage = document.createElement('marquee');
+//       errorMessage.textContent = 'Sorry, failed to fetch data...';
+//       app.appendChild(errorMessage);
+//   }
+// }
 
 
-request.send();
+// request.send();
